a51906103247a98f4b14453284e3f6d467a6a281
Bouchene Amjed Ziyad <ziyadamjed@gmail.com>
2026-04-29 23:11:31 +0200

    week8 day5: full airflow dbt pipeline orchestration

--- week6/dags/dbt_pipeline_dag.py
+++ week6/dags/dbt_pipeline_dag.py
@@ -10,10 +10,18 @@ with DAG(
 ) as dag:
     dbt_run = BashOperator(
         task_id="dbt_run",
-        bash_command="cd /opt/airflow/project/week7/de_journey && dbt run --profiles-dir /opt/airflow/project/week7/de_journey"
+        bash_command="cd /opt/airflow/project/week7/de_journey && dbt run --profiles-dir /opt/airflow/project/week7/de_journey --no-version-check"
     )
     dbt_test = BashOperator(
         task_id="dbt_test",
-        bash_command="cd /opt/airflow/project/week7/de_journey && dbt test --profiles-dir /opt/airflow/project/week7/de_journey"
+        bash_command="cd /opt/airflow/project/week7/de_journey && dbt test --profiles-dir /opt/airflow/project/week7/de_journey --no-version-check"
     )
-    dbt_run >> dbt_test
+    dbt_snapshot = BashOperator(
+        task_id ="dbt_snapshot",
+        bash_command = "cd /opt/airflow/project/week7/de_journey && dbt snapshot --profiles-dir /opt/airflow/project/week7/de_journey --no-version-check"
+    )
+    dbt_source_freshness = BashOperator(
+        task_id="dbt_source_freshness",
+        bash_command="cd /opt/airflow/project/week7/de_journey && dbt source freshness --profiles-dir /opt/airflow/project/week7/de_journey --no-version-check"
+    )
+    dbt_snapshot >> dbt_run >> dbt_test >> dbt_source_freshness
